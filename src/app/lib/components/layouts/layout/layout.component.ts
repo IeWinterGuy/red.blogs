553127e764822c7565e8a992f6e32af6aab08a93
ranvir jha <66358681+IWinterGuy@users.noreply.github.com>
2023-02-05 11:56:37 +0530

    simplified routing pages

--- src/app/lib/components/layouts/layout/layout.component.ts
+++ src/app/lib/components/layouts/layout/layout.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { FooterComponent, NavbarComponent } from '@lib/components';
 
 @Component({
   selector: 'app-layout-horizontal',
   standalone: true,
-  imports: [CommonModule, NavbarComponent, FooterComponent],
+  imports: [CommonModule, NavbarComponent, FooterComponent, RouterModule],
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
