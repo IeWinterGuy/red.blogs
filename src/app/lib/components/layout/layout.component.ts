32853fcd9de8109bd4fad4b4b17b58e9b03ef9be
ranvir jha <66358681+IWinterGuy@users.noreply.github.com>
2023-02-06 11:54:54 +0530

    gentle tree-shaking

--- src/app/lib/components/layout/layout.component.ts
+++ src/app/lib/components/layout/layout.component.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { FooterComponent, NavbarComponent } from '@lib/components';
+import { FooterComponent } from '@lib/components';
 
 @Component({
   selector: 'layout',
   standalone: true,
-  imports: [CommonModule, NavbarComponent, FooterComponent, RouterModule],
+  imports: [CommonModule, FooterComponent, RouterModule],
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
--- src/app/lib/interfaces/user.interface.ts
+++ src/app/lib/interfaces/user.interface.ts
@@ -3,3 +3,14 @@ export interface User {
   email: string;
   role: string;
 }
+
+export interface IUser {
+  nickname?: string;
+  name: string | undefined;
+  picture?: string;
+  updated_at?: string;
+  email?: string;
+  email_verified?: boolean;
+  sub?: string;
+  user_id?: string
+}
