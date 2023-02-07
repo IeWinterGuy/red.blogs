48117cc7941e84b221d11047cd65dfe334719b2e
ranvir jha <66358681+IWinterGuy@users.noreply.github.com>
2023-02-07 15:03:51 +0530

    ++ blog containers

--- src/app/pages/home/shared/articles/articles.component.ts
+++ src/app/pages/home/shared/articles/articles.component.ts
@@ -1,5 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { PublishNotes } from '@lib/interfaces';
+import { CreatepostService } from '@lib/services/firebase/createpost.service';
+import { PostComponent } from './post/post.component';
 
 
 @Component({
@@ -7,19 +11,22 @@ import { Component, OnInit } from '@angular/core';
   standalone: true,
   imports: [
     CommonModule,
-
+    PostComponent
   ],
   templateUrl: './articles.component.html',
   styleUrls: ['./articles.component.css']
 })
 export class ArticlesComponent implements OnInit {
-
-  constructor() {
+  public articles: PublishNotes[] = [];
+  constructor(public router: Router, public postServ: CreatepostService) {
     //..
   }
 
   ngOnInit(): void {
-
+    this.postServ.getArticlesFromFirebase().then((res) => {
+      this.articles = res;
+      console.log(this.articles)
+    })
   }
 
 }
---
+++ src/app/pages/home/shared/articles/post/post.component.ts
@@ -0,0 +1,34 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { IArticleMapper, PublishNotes } from '@lib/interfaces';
+import { ArticleMapper } from '@lib/mapper/article.mapper';
+
+@Component({
+  selector: 'post',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent implements OnInit {
+  @Input()
+  userNotesAndArticlesInfo: PublishNotes;
+  publicationHighlights!: IArticleMapper
+
+  constructor(public postMapper: ArticleMapper, private router: Router) {
+    this.userNotesAndArticlesInfo = <PublishNotes>{};
+    this.publicationHighlights = <IArticleMapper>{};
+  }
+
+  ngOnInit(): void {
+    this.postMapper.mapPublicationHighlights(this.userNotesAndArticlesInfo).then((res) => {
+      this.publicationHighlights = res;
+      console.log(res)
+    })
+  }
+
+  public onBtnActionClicked(id: string) {
+    this.router.navigate(['/publication/publish'], { queryParams: { page: id}});
+  }
+}
